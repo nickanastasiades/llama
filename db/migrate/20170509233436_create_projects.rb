203cabca4ff2be063c33ec0e6a79670263c678d3
nickanastasiades <Nick@Nicks-MacBook.local>
2017-05-09 20:27:23 -0500

    more edits

--- db/migrate/20170509233436_create_projects.rb
+++ db/migrate/20170509233436_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
       t.string :title
-      t.string :type
+      t.string :project_type
       t.string :description
       t.string :status
       t.float :version
