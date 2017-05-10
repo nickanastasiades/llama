f2045d9879f6107862dd272a4deafd0975df77f2
nickanastasiades <Nick@Nicks-MacBook.local>
2017-05-10 12:18:37 -0500

    remove these edits

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509235452) do
-
+ActiveRecord::Schema.define(version: 20170510170103) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -60,8 +59,12 @@ ActiveRecord::Schema.define(version: 20170509235452) do
     t.integer  "project_id"
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "projects", force: :cascade do |t|
